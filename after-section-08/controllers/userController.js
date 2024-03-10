413689a7e51fa0cc19e86194d1094d5bdf21547a
Huiyu-Huang-Brandies <147773120+Huiyu-Huang-Brandies@users.noreply.github.com>
2024-03-09 20:47:26 -0500

    factory functions: reading

--- after-section-08/controllers/userController.js
+++ after-section-08/controllers/userController.js
@@ -11,19 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) create error is user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -62,16 +49,10 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined! Please use /signup instead!'
   });
 };
 exports.updateUser = (req, res) => {
@@ -81,6 +62,8 @@ exports.updateUser = (req, res) => {
   });
 };
 
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 // do not update password with this. only for Admin using
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
