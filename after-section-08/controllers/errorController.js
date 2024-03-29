e55d027e910de7461315dc8caa276c7eef94bfa1
Huiyu-Huang-Brandies <147773120+Huiyu-Huang-Brandies@users.noreply.github.com>
2024-03-29 00:11:04 -0400

    render error page for prod and dev, both has two branch, one for developer and one for client

--- after-section-08/controllers/errorController.js
+++ after-section-08/controllers/errorController.js
@@ -24,33 +24,58 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError('Your token has expired! Please log in again.', 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
+const sendErrorDev = (err, req, res) => {
+  // when the Url starts with /api, send the error as JSON
+  if (req.originalUrl.startsWith('/api')) {
+    // originalUrl: the url without the host
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+    // if it isn't /api, we want to render the error -> rendered website
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: err.message
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-    // Programming or other unknown error: don)'t leak error details
-  } else {
+const sendErrorProd = (err, req, res) => {
+  // A) api
+  if (req.originalUrl.startsWith('/api')) {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    }
+    // B) Programming or other unknown error: don't leak error details
     // 1) log error
     console.error('ERROR: ', err);
-
     // 2) send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!'
     });
   }
+  // B) renderd website
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknown error: don)'t leak error details
+  }
+  // 1) log error
+  console.error('ERROR: ', err);
+  // 2) send generic message
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went very wrong!'
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -58,15 +83,19 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = { ...err }; // this don't work for .message,
+    // so we still need to make a copy here
+    error.message = err.message;
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    // console.log(err.message);
+    // console.log(error.message);
 
     sendErrorProd(error, res);
   }
--- after-section-08/controllers/viewsController.js
+++ after-section-08/controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -22,6 +23,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user'
   });
 
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
   // 2) build template
 
   // 3) render template using data from 1)
